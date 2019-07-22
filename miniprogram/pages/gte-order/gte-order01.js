7df5497cdbab9dcea4ce61833c8df50c3195ffbc
Davis <david8e307@126.com>
2019-07-22 19:20:05 +0800

    daivsadd

--- miniprogram/pages/gte-order/gte-order01.js
+++ miniprogram/pages/gte-order/gte-order01.js
@@ -7,12 +7,13 @@ Page({
   data: {
     pros: [{ vid: '01', name: "人气热卖" }, { vid: '02', name: "咖啡" }, { vid: '03', name: "沙拉" }, { vid: '04', name: "营养果餐" }, { vid: '05', name: "奶昔" }, { vid: '06', name: "欧包" }, { vid: '07', name: "便当" }],
     chose:'01',
+    intoview:"v01",
     count:0,
     pick:true,
     height:null,
     things:[
       {
-        id: "01", name:"人气热卖",thing:[
+        vid: "01", name:"人气热卖",thing:[
         { id:"0101",  name: "卤肉饭", price: 15.0},
         { id: "0102", name: "卤肉饭", price: 13.0},
         { id: "0103", name: "鸡肉饭", price: 13.5 },
@@ -23,10 +24,10 @@ Page({
         { id: "0108", name: "煲仔饭", price: 16.0},
       ]},
       {
-        id: "02", name: "咖啡", thing: [
-          { id: "0101", name: "卤肉饭", price: 15.0 },
-          { id: "0102", name: "卤肉饭", price: 13.0 },
-          { id: "0103", name: "鸡肉饭", price: 13.5 },
+        vid: "02", name: "咖啡", thing: [
+          { id: "0101", name: "去去去", price: 15.0 },
+          { id: "0102", name: "滚滚滚", price: 13.0 },
+          { id: "0103", name: "哈哈", price: 13.5 },
           { id: "0104", name: "牛肉饭", price: 15.0 },
           { id: "0105", name: "鸭肉饭", price: 9.0 },
           { id: "0106", name: "青蛙饭", price: 11.0 },
@@ -35,10 +36,10 @@ Page({
         ]
       },
       {
-        id: "03", name: "沙拉", thing: [
-          { id: "0101", name: "卤肉饭", price: 15.0 },
-          { id: "0102", name: "卤肉饭", price: 13.0 },
-          { id: "0103", name: "鸡肉饭", price: 13.5 },
+        vid: "03", name: "沙拉", thing: [
+          { id: "0101", name: "一i", price: 15.0 },
+          { id: "0102", name: "营养", price: 13.0 },
+          { id: "0103", name: "鸡肉买买买饭", price: 13.5 },
           { id: "0104", name: "牛肉饭", price: 15.0 },
           { id: "0105", name: "鸭肉饭", price: 9.0 },
           { id: "0106", name: "青蛙饭", price: 11.0 },
@@ -47,10 +48,34 @@ Page({
         ]
       },
       {
-        id: "04", name: "营养果餐", thing: [
-          { id: "0101", name: "卤肉饭", price: 15.0 },
-          { id: "0102", name: "卤肉饭", price: 13.0 },
+        vid: "04", name: "营养果餐", thing: [
+          { id: "0101", name: "咖啡", price: 15.0 },
+          { id: "0102", name: "可乐", price: 13.0 },
           { id: "0103", name: "鸡肉饭", price: 13.5 },
+          { id: "0104", name: "哈哈", price: 15.0 },
+          { id: "0105", name: "鸡翅", price: 9.0 },
+          { id: "0106", name: "青蛙饭", price: 11.0 },
+          { id: "0107", name: "鸽子饭", price: 21.5 },
+          { id: "0108", name: "煲仔饭", price: 16.0 },
+        ]
+      },
+      {
+        vid: "05", name: "奶昔", thing: [
+          { id: "0101", name: "问问", price: 15.0 },
+          { id: "0102", name: "是不", price: 13.0 },
+          { id: "0103", name: "的", price: 13.5 },
+          { id: "0104", name: "饭", price: 15.0 },
+          { id: "0105", name: "鸭肉饭", price: 9.0 },
+          { id: "0106", name: "青蛙饭", price: 11.0 },
+          { id: "0107", name: "鸽子饭", price: 21.5 },
+          { id: "0108", name: "煲仔饭", price: 16.0 },
+        ]
+      },
+      {
+        vid: "06", name: "欧包", thing: [
+          { id: "0101", name: "嗯嗯", price: 15.0 },
+          { id: "0102", name: "食物", price: 13.0 },
+          { id: "0103", name: "鸡肉的饭", price: 13.5 },
           { id: "0104", name: "牛肉饭", price: 15.0 },
           { id: "0105", name: "鸭肉饭", price: 9.0 },
           { id: "0106", name: "青蛙饭", price: 11.0 },
@@ -59,51 +84,35 @@ Page({
         ]
       },
       {
-        id: "05", name: "奶昔", thing: [
-          { id: "0101", name: "卤肉饭", price: 15.0 },
-          { id: "0102", name: "卤肉饭", price: 13.0 },
+        vid: "07", name: "便当", thing: [
+          { id: "0101", name: "青蛙", price: 15.0 },
+          { id: "0102", name: "个人", price: 13.0 },
           { id: "0103", name: "鸡肉饭", price: 13.5 },
-          { id: "0104", name: "牛肉饭", price: 15.0 },
+          { id: "0104", name: "哈哈就", price: 15.0 },
           { id: "0105", name: "鸭肉饭", price: 9.0 },
           { id: "0106", name: "青蛙饭", price: 11.0 },
           { id: "0107", name: "鸽子饭", price: 21.5 },
           { id: "0108", name: "煲仔饭", price: 16.0 },
         ]
       },
-      {
-        id: "06", name: "欧包", thing: [
-          { id: "0101", name: "卤肉饭", price: 15.0 },
-          { id: "0102", name: "卤肉饭", price: 13.0 },
-          { id: "0103", name: "鸡肉饭", price: 13.5 },
-          { id: "0104", name: "牛肉饭", price: 15.0 },
-          { id: "0105", name: "鸭肉饭", price: 9.0 },
-          { id: "0106", name: "青蛙饭", price: 11.0 },
-          { id: "0107", name: "鸽子饭", price: 21.5 },
-          { id: "0108", name: "煲仔饭", price: 16.0 },
-        ]
-      },
-      {
-        id: "07", name: "便当", thing: [
-          { id: "0101", name: "卤肉饭", price: 15.0 },
-          { id: "0102", name: "卤肉饭", price: 13.0 },
-          { id: "0103", name: "鸡肉饭", price: 13.5 },
-          { id: "0104", name: "牛肉饭", price: 15.0 },
-          { id: "0105", name: "鸭肉饭", price: 9.0 },
-          { id: "0106", name: "青蛙饭", price: 11.0 },
-          { id: "0107", name: "鸽子饭", price: 21.5 },
-          { id: "0108", name: "煲仔饭", price: 16.0 },
-        ]
-      },
-
     ],
-    shopbag:[]
-
+    shopbag:[],
+    v1top:0,
+    v2top: 0,
+    v3top: 0,
+    v4top: 0,
+    v5top: 0,
+    v6top: 0,
+    v7top: 0,
   },
   change:function(e){
    var vid=e.target.dataset.vid
+   var into="v"+vid
    this.setData({
-     chose:vid
+     chose:vid,
+     intoview:into
    })
+   console.log(this.data.intoview)
   },
   pick:function(){
     if(this.data.pick===true){
@@ -129,26 +138,42 @@ Page({
  query:function(){
    var that=this;
    const query = wx.createSelectorQuery()                // 创建节点查询器 query
-   query.select('#title').boundingClientRect()    // 这段代码的意思是选择Id=productServe的节点，获取节点位置信息的查询请求
-   query.select('#middle').boundingClientRect() // 这段代码的意思是选择Id=enterpriseServe的节点，获取节点位置信息的查询请求
-   query.select('#bot').boundingClientRect()     // 这段代码的意思是选择Id=normalServe的节点，获取节点位置信息的查询请求
-   query.select('#outside').boundingClientRect()   
-   query.selectViewport().scrollOffset()                 // 这段代码的意思是获取页面滑动位置的查询请求
-   query.exec((res) => {
+   new Promise(open=>{
+     query.select('#title').boundingClientRect()    // 这段代码的意思是选择Id=productServe的节点，获取节点位置信息的查询请求
+     query.select('#middle').boundingClientRect() // 这段代码的意思是选择Id=enterpriseServe的节点，获取节点位置信息的查询请求
+     query.select('#bot').boundingClientRect()     // 这段代码的意思是选择Id=normalServe的节点，获取节点位置信息的查询请求
+     query.select('#v01').boundingClientRect() 
+     query.select('#v02').boundingClientRect() 
+     query.select('#v03').boundingClientRect() 
+     query.select('#v04').boundingClientRect() 
+     query.select('#v05').boundingClientRect() 
+     query.select('#v06').boundingClientRect() 
+     query.select('#v07').boundingClientRect() 
+
+     query.select('#outside').boundingClientRect();
+     query.selectViewport().scrollOffset()  ; 
+      // 这段代码的意思是获取页面滑动位置的查询请求
+     query.exec(function (res) {
+       open(res)
+     })             
+   }).then(res=>{
      console.log(res)
-     console.log(res[3].height)
-     console.log(that.data.height)
-    var newheight=that.data.height-res[0].height-res[2].height;
-    console.log(newheight)
-    that.setData({
-      height:newheight
-    })
-   })
+       var newheight = that.data.height - res[0].height - res[2].height;
+       that.setData({
+         height: newheight,
+          v1top:res[3].top,
+         v2top: res[4].top,
+         v3top: res[5].top,
+         v4top: res[6].top,
+         v5top: res[7].top,
+         v6top: res[8].top,
+         v7top: res[9].top,
+       })
+     })
+   
  },
 
- onPageScroll:function(e){
-   console.log(e.detail.scrollTop)
- },
+ 
  //商品数量-加入购物车控制
  qtyCtrlup(e){ 
    var obj = { id: e.target.dataset.id, name: e.target.dataset.name, qty: 1, price: e.target.dataset.price,}
@@ -205,6 +230,43 @@ Page({
      count:ncount
    })
  },
+ //滚动事件
+ onscroll:function(e){
+   
+   var scrolltop = e.detail.scrollTop+100;
+   
+   if (scrolltop >= this.data.v1top && scrolltop <= this.data.v2top){
+     this.setData({
+       chose: "01",
+     }) 
+   } else if (scrolltop >= this.data.v2top && scrolltop <= this.data.v3top){
+     this.setData({
+       chose: "02",
+     })
+    
+   } else if (scrolltop >= this.data.v3top && scrolltop <= this.data.v4top) {
+     this.setData({
+       chose: "03",
+     })
+   } else if (scrolltop >= this.data.v4top && scrolltop <= this.data.v5top) {
+     this.setData({
+       chose: "04",
+     })
+     
+   } else if (scrolltop >= this.data.v5top && scrolltop <= this.data.v6top) {
+    this.setData({
+       chose: "05",
+     })
+   } else if (scrolltop >= this.data.v6top && scrolltop <= this.data.v7top) {
+     this.setData({
+       chose: "06",
+     })
+   } else if ( scrolltop >= this.data.v7top) {
+     this.setData({
+       chose: "07",
+     })
+   };
+ },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -217,7 +279,7 @@ Page({
    */
   onReady: function () {
     // 获取节点信息最好延迟获取
-    setTimeout(()=>{this.query();},100)
+    this.query()
   },
 
   /**
